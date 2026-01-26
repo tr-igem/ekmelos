4d32bff68af792f2c82e07cae63e37b0390efca1
Thomas Richter <164681047+tr-igem@users.noreply.github.com>
2026-01-26 04:36:43 +0100

    Add codepoint, title, desc

--- scripts/svg.py
+++ scripts/svg.py
@@ -4,49 +4,59 @@
 ##
 ## svg.py
 ##
-## Script for FontForge generating an SVG file for one or more glyphs
-## in the currently active font;
-## either the selected glyphs or the glyphs listed in the file
-## "FONTPATH/svg/svg.txt" (one glyph name per line).
+## Script for FontForge generating an SVG file with one or more glyphs
+## in the currently active font.
+## Takes either the selected glyphs or the glyphs listed in the file
+## "FONTPATH/svg/svg.txt".
+## Format: Each line is one of:
+##    = TITLE
+##    == DESCRIPTION
+##    Glyphname
+##    #xCodepoint or U+Codepoint
+##    Empty or with ## (comment), is ignored
 ##
-## Reads "FONTPATH/svg/template.svg" and saves it with the
-## included glyphs under "FONTPATH/svg/FILENAME.svg".
-## FILENAME is the glyph name of the first glyph.
+## Reads "FONTPATH/svg/template.svg" and saves it with the glyphs
+## under "FONTPATH/svg/FILENAME.svg".
+## FILENAME is TITLE in kebab-case, or the glyphname or codepoint
+## of the first glyph.
 ##
 ## The template file may include the following replacement fields
 ## with overall data:
 ##
 ##  {0.font}        Font name (fullname)
 ##  {0.copyright}   Font copyright notice
-##  {0.comment}     Font comment
+##  {0.filename}    FILENAME
+##  {0.title}       TITLE
+##  {0.desc}        Font comment or DESCRIPTION
 ##  {0.ascent}      Extent acc. to all glyph bounding boxes
 ##  {0.descent}
 ##  {0.width}
 ##  {0.height}
 ##  {0.max_height}  Maximum height of all glyphs
 ##  {0.x}           Position for the next glyph
-##  {0.path}        Sequence of "<path.../>" elements of all glyphs
+##  {0.path}        Sequence of <path> elements of all glyphs
 ##
-## The first "<path.../>" element in the template file is used
-## for each glyph. It is replaced internally with "{0.path}".
+## The first <path> element in the template file is used for each glyph.
+## It is replaced internally with "{0.path}".
 ## It may include the above and the following replacement fields
 ## with glyph specific data:
 ##
-##  {1.unicode}     Code point
-##  {1.code}        Code point as hex string
-##  {1.name}        Glyph name
+##  {1.unicode}     Codepoint
+##  {1.code}        Codepoint as hex string
+##  {1.name}        Glyphname
 ##  {1.desc}        Comment
 ##  {1.ascent}      Extent acc. to bounding box
 ##  {1.descent}
 ##  {1.width}
 ##  {1.height}
-##  {1.path}        SVG path (for attribute "d")
+##  {1.path}        SVG path, for attribute "d"
 ##
-## The glyphs are stacked in a line with a padding. Default is 1/20 em.
-## Another padding can be specified immediately after the "<path.../>" element.
+## The glyphs are stacked in a line with a padding of 1/20 em.
+## Another padding can be specified immediately after the <path> element.
 ##
 ##
 ## Written by Thomas Richter (thomas-richter@aon.at), 2026-01-25
+## 2026-01-26: Add codepoint, title, desc.
 ##
 ## This program is free software. Use, redistribute, and modify it as you wish.
 ##
@@ -58,7 +68,6 @@ font = fontforge.activeFont()
 basepath = font.path.rsplit("/", 1)[0]
 svgpath = basepath + "/svg"
 
-filename = None
 glyphnames = []
 
 pathElement = '''\
@@ -66,15 +75,16 @@ pathElement = '''\
     d="{1.path}"
   />'''
 
-padding = int(font.em / 20)
-
 
 class SvgData:
     """Overall data for the SVG file"""
     def __init__(self, font):
         self.font = font.fullname
         self.copyright = font.copyright
-        self.comment = font.comment
+        self.filename = None
+        self.title = None
+        self.desc = font.comment
+        self.pad = int(font.em / 20)
         self.glyphs = []
         self.ascent = 0
         self.descent = 0
@@ -84,15 +94,26 @@ class SvgData:
         self.x = 0
         self.path = ''
 
-    def add(self, g):
+    def name(self, n):
+        n = n.strip()
+        if not self.title:
+            self.title = n
+        if not self.filename:
+            self.filename = "-".join(n.casefold().split())
+
+    def add(self, g, n):
+        if g.layers[1].isEmpty():
+            return
+        g = SvgGlyph(g)
         self.glyphs.append(g)
         self.ascent = max(self.ascent , g.ascent)
         self.descent = min(self.descent, g.descent)
         self.height = int(self.ascent - self.descent)
         self.max_height = max(self.max_height, g.height)
-        if self.width > 0: self.x = self.width + padding
+        if self.width > 0: self.x = self.width + self.pad
         self.width = self.x + g.width
         g.format(self)
+        self.name(n)
 
     def finish(self):
         self.path = "\n".join([str(g) for g in self.glyphs])
@@ -154,6 +175,9 @@ class SvgGlyph:
         return self.path_element
 
 
+svg = SvgData(font)
+
+
 ## read list of glyphs
 
 path = svgpath + "/svg.txt"
@@ -163,10 +187,13 @@ except OSError:
     pass
 else:
     for l in file:
-        if "##" in l:
-            break
-        elif "#" in l:
+        if l.isspace() or "##" in l:
             continue
+        elif l.startswith("="):
+            if l.startswith("=="):
+                svg.desc = l[2:].strip()
+            else:
+                svg.name(l[1:])
         else:
             glyphnames.append(l.strip())
     file.close()
@@ -189,35 +216,33 @@ if len(p[1]) != 0:
             else: break
         tpl = p[0] + "{0.path}" + e[2][len(n):]
         pathElement = p[1] + e[0] + e[1]
-        if len(n) != 0: padding = int(n)
+        if len(n) != 0:
+            svg.pad = int(n)
 
 
 ## generate SVG for all glyphs
 
-svg = SvgData(font)
-
 if len(glyphnames) == 0:
     for g in font.selection.byGlyphs:
-        if not filename:
-            filename = g.glyphname
-        if not g.layers[1].isEmpty():
-            svg.add(SvgGlyph(g))
+        svg.add(g, g.glyphname)
 else:
     for n in glyphnames:
         try:
-            g = font[n]
+            if n.startswith(("#x", "U+")):
+                n = n[2:]
+                g = font[int(n, 16)]
+            else:
+                g = font[n]
         except Exception:
             continue
-        if not filename:
-            filename = n
-        if not g.layers[1].isEmpty():
-            svg.add(SvgGlyph(g))
+        else:
+            svg.add(g, n)
 
 svg.finish()
 
 
-if not filename: filename = 'glyphs'
-path = svgpath + "/" + filename + ".svg"
-file = open(path, 'w', newline = "\n")
-file.write(tpl.format(svg))
-file.close()
+if svg.filename:
+    path = svgpath + "/" + svg.filename + ".svg"
+    file = open(path, 'w', newline = "\n")
+    file.write(tpl.format(svg))
+    file.close()
